3a5420f015f3374bb5fc8434b54abea5132638a1
mnp014 <rocks.pradeep014@gmail.com>
2017-11-16 14:12:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication                //tells this is a spring boot application and this is the starting point of application.
-public class CourseApiApp {
+public class HelloWorld {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApiApp.class, args);   // creates a servlet container and host this application in this servlet container 
+		SpringApplication.run(HelloWorld.class, args);   // creates a servlet container and host this application in this servlet container 
 	}
 }
