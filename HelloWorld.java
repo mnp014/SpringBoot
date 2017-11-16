55fecbc99541b89f305762a6b63e86d65c131967
mnp014 <rocks.pradeep014@gmail.com>
2017-11-16 14:17:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorld.class, args);   // creates a servlet container and host this application in this servlet container 
+	//SpringApplication.run(class, args) is a static method which takes cmd line args and a class which has an annotation  as arguments 
 	}
 }
