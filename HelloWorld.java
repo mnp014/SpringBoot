fd30c4a9ca21351e1272a4c72a2170dc405000bc
mnp014 <rocks.pradeep014@gmail.com>
2017-11-16 14:07:39 +0530

    Create HelloWorld.java
    How to tell the compiler that this is a spring boot application?? and 
    Which is the starting point of the application?? and  
    How to creates a servlet container? and 
    How to host this application in this servlet container??

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package xyz.prazy.springbootStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication                //tells this is a spring boot application and this is the starting point of application.
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);   // creates a servlet container and host this application in this servlet container 
+	}
+}
