b950f1ffef1aaf3ca64829f6c92d1c2434cc660e
mnp014 <rocks.pradeep014@gmail.com>
2017-11-17 15:39:16 +0530

    Update TopicController.java

--- TopicController.java
+++ TopicController.java
@@ -5,39 +5,42 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
 	
+	private static final String[] method = null;
 	@Autowired
 	private TopicService topicService;     // "@Aurowired" marks "TopicService" as "needs DI"
 	
 	
 	
 	
-	
-	@RequestMapping("/topics")
+	/* GET REQUEST1*/
+	@RequestMapping("/topics")    // Map this method to any request that is a GET and has value "/topics"
 	public List<Topic> getAllTopics() {
     		return topicService.getAllTopics();
 	}
 	
-	//@RequestMapping("/topics/a")
+	
+	/* GET REQUEST2*/
+	//@RequestMapping("/topics/a")             // Map this method to any request that is a GET and has value "/topics"
 	@RequestMapping("/topics/{id}")           //{} represents a variable
 	public Topic getTopic(@PathVariable String id){                       //@Pathvariable tells this argument value 'id' contains value of variable in the path "/topics/{id}"
 		return topicService.getTopic(id);
 	}
 	
-
 	
-/*Alternate method 	*/
-	// if path variable and argument value are different (NOT RECOMMENDED) 
-//	@RequestMapping("/topics/{foo}")           //{} represents a variable
-//	public Topic getTopic2(@PathVariable ("foo") String id){                       //@Pathvariable tells this argument value 'id' contains value of variable in the path "/topics/{id}"
-//		return topicService.getTopic(id);
-//	}
+	/* POST REQUEST*/
+	@RequestMapping(method=RequestMethod.POST, value= "/topics")           // Map this method to any request that is a POST and has value "/topics"
+		public void addTopic(@RequestBody Topic topic){
+		topicService.addTopic(topic);
+			}
 	
 	
 	
