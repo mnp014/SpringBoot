ce63a32ae073f5a272ebada263dc1b81b3f0d089
mnp014 <rocks.pradeep014@gmail.com>
2017-11-17 14:54:28 +0530

    Update TopicController.java

--- TopicController.java
+++ TopicController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,31 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;     // "@Aurowired" marks "TopicService" as "needs DI"
 	
-		
+	
+	
+	
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-        
-		
-		return topicService.getAllTopics();
-				 
+    		return topicService.getAllTopics();
 	}
+	
+	//@RequestMapping("/topics/a")
+	@RequestMapping("/topics/{id}")           //{} represents a variable
+	public Topic getTopic(@PathVariable String id){                       //@Pathvariable tells this argument value 'id' contains value of variable in the path "/topics/{id}"
+		return topicService.getTopic(id);
+	}
+	
+
+	
+/*Alternate method 	*/
+	// if path variable and argument value are different (NOT RECOMMENDED) 
+//	@RequestMapping("/topics/{foo}")           //{} represents a variable
+//	public Topic getTopic2(@PathVariable ("foo") String id){                       //@Pathvariable tells this argument value 'id' contains value of variable in the path "/topics/{id}"
+//		return topicService.getTopic(id);
+//	}
+	
+	
+	
+	
 }
