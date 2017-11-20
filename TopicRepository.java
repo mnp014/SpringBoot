d17592a9bdecc3e1bacf9e1b65306547c90d899f
mnp014 <rocks.pradeep014@gmail.com>
2017-11-20 15:49:29 +0530

    Delete TopicRepository.java

--- TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package xyz.prazy.springbootStarter.Topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String>{                    //Topic & String are generic types
-
-	
-}
